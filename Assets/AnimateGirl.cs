3ca6680cb4890138fbda735fd46ecd20447e32a6
Tardif <dylan.tardif1@gmail.com>
2020-09-30 16:58:47 +0200

    Add tracker

--- Assets/AnimateGirl.cs
+++ Assets/AnimateGirl.cs
@@ -7,37 +7,49 @@ public class AnimateGirl : MonoBehaviour {
 
     Animator animator;
     SpriteRenderer spriteRenderer;
-
-    
+    public float maxSpeed = 2;
     private void Start() {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
+        var maxDistancePerFrame = maxSpeed * Time.deltaTime;
+        Vector3 move = Vector3.zero;
+
         if (Input.GetKey(KeyCode.RightArrow)){
             animator.SetFloat("Speed", 2f);
-            this.transform.position = this.transform.position + ;
             spriteRenderer.flipX = false;
-        } else if(Input.GetKey(KeyCode.LeftArrow)) {
-                animator.SetFloat("Speed", 2f);
-                spriteRenderer.flipX = true;
-            }   else {
-                    animator.SetFloat("Speed", 0f);
-                }
-        
-        if(Input.GetKey(KeyCode.UpArrow)) {
+            move += Vector3.right * maxDistancePerFrame;
+        }
+
+        else if(Input.GetKey(KeyCode.LeftArrow)) 
+        {
             animator.SetFloat("Speed", 2f);
-            animator.SetInteger("Direction", 1);
-        } else {
+            spriteRenderer.flipX = true;
+            move += Vector3.left * maxDistancePerFrame;
+        }
+        
+        else 
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+        
+        if(Input.GetKey(KeyCode.UpArrow)) 
+        {
+            move += Vector3.up * maxDistancePerFrame;
+        } 
+
+        else if(Input.GetKey(KeyCode.DownArrow)) 
+        {
+            move += Vector3.down * maxDistancePerFrame;
+        } 
+
+        else 
+        {
             animator.SetInteger("Direction", 0);
         }
 
-        if(Input.GetKey(KeyCode.DownArrow)) {
-            animator.SetFloat("Speed", 2f);
-            animator.SetInteger("Direction", 2);
-        } else {
-            animator.SetInteger("Direction", 0);
-        }
+        this.transform.position = this.transform.position + move;
     }
 }
