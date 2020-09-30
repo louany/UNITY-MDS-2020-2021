4eea4fecf021f2df1c23ba3fcd2f0c11f22197b7
Tardif <dylan.tardif1@gmail.com>
2020-09-30 16:10:20 +0200

    first commit

---
+++ Assets/AnimateGirl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator),typeof(SpriteRenderer))]
+public class AnimateGirl : MonoBehaviour {
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    
+    private void Start() {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            animator.SetFloat("Speed", 2f);
+            this.transform.position = this.transform.position + ;
+            spriteRenderer.flipX = false;
+        } else if(Input.GetKey(KeyCode.LeftArrow)) {
+                animator.SetFloat("Speed", 2f);
+                spriteRenderer.flipX = true;
+            }   else {
+                    animator.SetFloat("Speed", 0f);
+                }
+        
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            animator.SetFloat("Speed", 2f);
+            animator.SetInteger("Direction", 1);
+        } else {
+            animator.SetInteger("Direction", 0);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            animator.SetFloat("Speed", 2f);
+            animator.SetInteger("Direction", 2);
+        } else {
+            animator.SetInteger("Direction", 0);
+        }
+    }
+}
